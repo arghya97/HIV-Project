fd28473e37bbf304235fd34faba0d0d3aeabf670
Arghyadip Manna <arghyadipmanna03@gmail.com>
2019-03-08 18:59:04 +0530

    fixed error in clean urls file

--- newsday/clean_links.py
+++ newsday/clean_links.py
@@ -3,8 +3,12 @@ with open('article_links.txt','r') as fl:
 
 mod = []
 for link in links:
-    if link !='$content.urlLink.value':
-        mod.append(link)
+    if link !='$content.urlLink.value\n':
+        if 'https://' not in link:
+            link_mod = 'https'+link[4:]
+            mod.append(link_mod)
+        else:
+            mod.append(link)
 
 with open('article_links_mod.txt','w') as fl:
     fl.write(''.join(mod))
