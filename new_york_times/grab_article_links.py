1ed6f83fe44755d93f9098241e86d82aeb90c332
Arghyadip Manna <arghyadipmanna03@gmail.com>
2019-03-08 17:12:35 +0530

    used chrome instead of firefox

--- new_york_times/grab_article_links.py
+++ new_york_times/grab_article_links.py
@@ -9,7 +9,11 @@ def modify_links(links):
             links[i]= path+links[i]
     return links
 def get_driver(link):
-    driver = webdriver.Firefox()
+    from selenium.webdriver.chrome.options import Options
+
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(options=options)
     driver.get(link)
     return driver
 def load_page(driver):
@@ -19,7 +23,7 @@ def load_page(driver):
         t+=1
         try:
             driver.find_element_by_xpath('//*[@id="site-content"]/div/div[2]/div[3]/div/button').click()
-            sleep(5)
+            sleep(2)
         except:
             found = False
         print(t,end='\r')
@@ -27,16 +31,17 @@ def load_page(driver):
     driver.quit()
     return page
 def get_links(html):
+    links = []
     soup = bs(html,'html.parser')
     articles = soup.find_all('div',class_='css-138we14')
-    links = []
+    
     for article in articles:
         lnk = article.a.get('href').strip()
         links.append(lnk)
     return links
 
 
-link = 'https://www.nytimes.com/search?query=HIV&sort=oldest&startDate=20000101'
+link = 'https://www.nytimes.com/search?query=HIV&sort=best&startDate=20000101'
 driver = get_driver(link)
 
 loaded_page = load_page(driver)
